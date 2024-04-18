f7004d5b6cc29324c78da2310dbc60ffff07b469
aditya kelkar <aditya.kelkar@fleetcor.com>
2024-04-18 08:22:54 -0400

    Adding sandbox03

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,30 @@ module "sandbox02" {
   custom_fields = {
     group = "non-prod"
   }
+}
+module "sandbox03" {
+  source = "./modules/aft-account-request"
 
-  account_customizations_name = "sandbox"
+  control_tower_parameters = {
+    AccountEmail              = "adipurush.aws+sandbox3@gmail.com"
+    AccountName               = "sandbox03-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "adipurush.aws@gmail.com"
+    SSOUserFirstName          = "Sandbox03"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+    "type"           = "Sandbox03"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Aditya"
+    change_reason       = "Adding sandbox03"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
 }
