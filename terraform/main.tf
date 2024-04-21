da497c91cde5e066e3f3bf490c5adb1ae42822a9
adipurushAWS <167025007+adipurushAWS@users.noreply.github.com>
2024-04-20 21:01:07 -0400

    Append new Terraform module for sandbox04-aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,26 @@ module "sandbox03-aft" {
     change_reason       = "Agent based AFT account creation"
   }
 }
+
+
+module "sandbox04-aft" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "adipurush.aws+sandbox04@gmail.com"
+    AccountName               = "sandbox04-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "adipurush.aws+sandbox04@gmail.com"
+    SSOUserFirstName          = "Sandbox04"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "sandbox04-aft"
+    change_reason       = "Agent based AFT account creation"
+  }
+}
