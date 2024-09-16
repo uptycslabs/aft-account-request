0265626fa56aa400fd130280f11cccb7692e2ec4
gkondisetty-uptycs <gkondisetty@uptycs.com>
2024-09-16 15:15:40 -0400

    Added uptycs-cspm.tf

---
+++ terraform/uptycs-cspm.tf
@@ -0,0 +1,60 @@
+locals {
+  accounts = [
+    {
+      accountName       = "cloudquery-root-management"
+      integrationPrefix = "debug-11"
+      externalId        = "012e935c-45a7-5c63-936d-924967513211"
+    },
+    {
+      accountName       = "cqmanagement-account-1"
+      integrationPrefix = "debug-22"
+      externalId        = "012e935c-45a7-5c63-936d-924967513222"
+    },
+    {
+      accountName       = "cqmanagement-account-2"
+      integrationPrefix = "debug-33"
+      externalId        = "012e935c-45a7-5c63-936d-924967513233"
+    },
+    {
+      accountName       = "childaccount2"
+      integrationPrefix = "debug-44"
+      externalId        = "012e935c-45a7-5c63-936d-924967513244"
+    },
+    {
+      accountName       = "childaccount3"
+      integrationPrefix = "debug-55"
+      externalId        = "012e935c-45a7-5c63-936d-924967513255"
+    }
+  ]
+}
+
+provider "aws" {
+  alias = "management"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  for_each = { for account in local.accounts : account.accountName => account }
+  
+  alias = each.key
+  region = "us-east-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::${each.value.accountName}:role/AFTExecutionRole"
+    external_id  = each.value.externalId
+  }
+}
+
+module "uptycs_aws_cspm" {
+  source = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration/accounts"
+
+  for_each = { for account in local.accounts : account.accountName => account }
+
+  providers = {
+    aws = aws.each.key  # This ensures Terraform uses the correct provider alias for each account
+  }
+
+  upt_account_id     = "685272795239"
+  integration_prefix = each.value.integrationPrefix
+  external_id        = each.value.externalId
+}
