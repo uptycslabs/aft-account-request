7d7257bf5866f9c7f296ab4a5bebbcbf661b9aaa
gkondisetty-uptycs <gkondisetty@uptycs.com>
2024-09-16 13:54:07 -0400

    removed uptycs-cspm

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,74 +1,29 @@
 locals {
     accounts = [
         {
-            accountId                 = "924967513276"
             accountName               = "cqmanagement-account-1"
             accountEmail              = "aws-qa+cloudquery-cqmanagement-account-1@uptycs.com"
             managedOrganizationalUnit = "AdditionalOU"
-            ssoUserEmail              = "gkondisetty@uptycs.com"
-            ssoUserFirstName          = "Giri"
-            ssoUserLastName           = "Kondisetty"
-            accountTags               = { "Sandbox" = "cqmanagement-account-1 - 924967513276" }
-            changeRequestedBy         = "gkondisetty@uptycs.com"
-            changeReason              = "Import cqmanagement-account-1 in AFT"
-            customFieldsGroup         = "sandbox"
-            accountCustomizationsName = "sandbox"
         },
         {
-            accountId                 = "496028668001"
             accountName               = "cqmanagement-account-2"
             accountEmail              = "aws-qa+cloudquery-cqmanagement-account-2@uptycs.com"
             managedOrganizationalUnit = "QA-testing"
-            ssoUserEmail              = "gkondisetty@uptycs.com"
-            ssoUserFirstName          = "Giri"
-            ssoUserLastName           = "Kondisetty"
-            accountTags               = { "Sandbox" = "cqmanagement-account-2 - 496028668001" }
-            changeRequestedBy         = "gkondisetty@uptycs.com"
-            changeReason              = "Import cqmanagement-account-2 in AFT"
-            customFieldsGroup         = "sandbox"
-            accountCustomizationsName = "sandbox"
         },
         {
-            accountId                 = "912326510691"
             accountName               = "childaccount2"
             accountEmail              = "hpawar+2@uptycs.com"
             managedOrganizationalUnit = "QA-testing"
-            ssoUserEmail              = "gkondisetty@uptycs.com"
-            ssoUserFirstName          = "Giri"
-            ssoUserLastName           = "Kondisetty"
-            accountTags               = { "Sandbox" = "childaccount2 - 912326510691" }
-            changeRequestedBy         = "gkondisetty@uptycs.com"
-            changeReason              = "Import childaccount2 in AFT"
-            customFieldsGroup         = "sandbox"
-            accountCustomizationsName = "sandbox"
         },
         {
-            accountId                 = "051315720372"
             accountName               = "childaccount3"
             accountEmail              = "hpawar+3@uptycs.com"
             managedOrganizationalUnit = "QA-OU2"
-            ssoUserEmail              = "gkondisetty@uptycs.com"
-            ssoUserFirstName          = "Giri"
-            ssoUserLastName           = "Kondisetty"
-            accountTags               = { "Sandbox" = "childaccount3 - 051315720372" }
-            changeRequestedBy         = "gkondisetty@uptycs.com"
-            changeReason              = "Import childaccount3 in AFT"
-            customFieldsGroup         = "sandbox"
-            accountCustomizationsName = "sandbox"
         },
         {
-            accountId                 = "646602658615"
             accountName               = "cloudquery-root-management"
             accountEmail              = "aws-qa+cloudquery-root-management@uptycs.com"
             managedOrganizationalUnit = "Root"
-            ssoUserEmail              = "gkondisetty@uptycs.com"
-            ssoUserFirstName          = "Giri"
-            ssoUserLastName           = "Kondisetty"
-            accountTags               = { "Sandbox" = "cloudquery-root-management - 646602658615" }
-            changeRequestedBy         = "gkondisetty@uptycs.com"
-            changeReason              = "Import cloudquery-root-management in AFT"
-            customFieldsGroup         = "sandbox"
-            accountCustomizationsName = "sandbox"
         }
     ]
 }
@@ -79,24 +34,24 @@ module "aft_import_accounts" {
   for_each = { for account in local.accounts : account.accountName => account }
 
   control_tower_parameters = {
-    AccountEmail              = each.value.accountEmail
     AccountName               = each.value.accountName
+    AccountEmail              = each.value.accountEmail
     ManagedOrganizationalUnit = each.value.managedOrganizationalUnit
-    SSOUserEmail              = each.value.ssoUserEmail
-    SSOUserFirstName          = each.value.ssoUserFirstName
-    SSOUserLastName           = each.value.ssoUserLastName
+    SSOUserEmail              = "gkondisetty@uptycs.com"
+    SSOUserFirstName          = "Giri"
+    SSOUserLastName           = "Kondisetty"
   }
 
-  account_tags = each.value.accountTags
+  account_tags = { "Sandbox" = "${each.value.accountName}" }
 
   change_management_parameters = {
-    change_requested_by = each.value.changeRequestedBy
-    change_reason       = each.value.changeReason
+    change_requested_by = "gkondisetty@uptycs.com"
+    change_reason       = "Import ${each.value.accountName} in AFT"
   }
 
   custom_fields = {
-    group = each.value.customFieldsGroup
+    group = "sandbox"
   }
 
-  account_customizations_name = each.value.accountCustomizationsName
+  account_customizations_name = "sandbox"
 }
