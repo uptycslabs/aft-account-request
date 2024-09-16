33f79ea0c3dc8021ac1f4e2b4fdc492a74422a5c
gkondisetty-uptycs <gkondisetty@uptycs.com>
2024-09-16 10:55:59 -0400

    Added 4 Accounts to AFT
    Added 4 Accounts to AFT
    - All these 4 accounts are onboarded with Uptycs
    - 2 Accounts with CloudFormation
    - 2 Accounts with Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,105 @@
+variable "accounts" {
+  description = "List of account details"
+  type = list(object({
+    accountEmail               = string
+    accountName                = string
+    managedOrganizationalUnit  = string
+    ssoUserEmail               = string
+    ssoUserFirstName           = string
+    ssoUserLastName            = string
+    accountTags                = map(string)
+    changeRequestedBy          = string
+    changeReason               = string
+    customFieldsGroup          = string
+    accountCustomizationsName  = string
+  }))
+}
+
+locals {
+    accounts = [
+        {
+            accountId                 = "924967513276"
+            accountName               = "cqmanagement-account-1"
+            accountEmail              = "aws-qa+cloudquery-cqmanagement-account-1@uptycs.com"
+            managedOrganizationalUnit = "AdditionalOU"
+            ssoUserEmail              = "gkondisetty@uptycs.com"
+            ssoUserFirstName          = "Giri"
+            ssoUserLastName           = "Kondisetty"
+            accountTags               = { "Sandbox" = "cqmanagement-account-1 - 924967513276" }
+            changeRequestedBy         = "gkondisetty@uptycs.com"
+            changeReason              = "Import cqmanagement-account-1 in AFT"
+            customFieldsGroup         = "sandbox"
+            accountCustomizationsName = "sandbox"
+        },
+        {
+            accountId                 = "496028668001"
+            accountName               = "cqmanagement-account-2"
+            accountEmail              = "aws-qa+cloudquery-cqmanagement-account-2@uptycs.com"
+            managedOrganizationalUnit = "QA-testing"
+            ssoUserEmail              = "gkondisetty@uptycs.com"
+            ssoUserFirstName          = "Giri"
+            ssoUserLastName           = "Kondisetty"
+            accountTags              = { "Sandbox" = "cqmanagement-account-2 - 496028668001" }
+            changeRequestedBy        = "gkondisetty@uptycs.com"
+            changeReason              = "Import Account1 in AFT"
+            customFieldsGroup         = "sandbox"
+            accountCustomizationsName = "sandbox"
+        },
+        {
+            accountId                 = "912326510691"
+            accountName               = "childaccount2"
+            accountEmail              = "hpawar+2@uptycs.com"
+            managedOrganizationalUnit = "QA-testing"
+            ssoUserEmail              = "gkondisetty@uptycs.com"
+            ssoUserFirstName          = "Giri"
+            ssoUserLastName           = "Kondisetty"
+            accountTags               = { "Sandbox" = "childaccount2 - 912326510691" }
+            changeRequestedBy         = "gkondisetty@uptycs.com"
+            changeReason              = "Import childaccount2 in AFT"
+            customFieldsGroup         = "sandbox"
+            accountCustomizationsName = "sandbox"
+        },
+        {
+            accountId                 = "051315720372"
+            accountName               = "childaccount3"
+            accountEmail              = "hpawar+3@uptycs.com"
+            managedOrganizationalUnit = "QA-OU2"
+            ssoUserEmail              = "gkondisetty@uptycs.com"
+            ssoUserFirstName          = "Giri"
+            ssoUserLastName           = "Kondisetty"
+            accountTags               = { "Sandbox" = "childaccount3 - 051315720372" }
+            changeRequestedBy         = "gkondisetty@uptycs.com"
+            changeReason              = "Import childaccount3 in AFT"
+            customFieldsGroup         = "sandbox"
+            accountCustomizationsName = "sandbox"
+        }
+    ]
+}
+
+module "aft_import_accounts" {
+  source = "./modules/aft-account-request"
+
+  for_each = { for account in var.accounts : account.account_name => account }
+
+  control_tower_parameters = {
+    AccountEmail              = each.value.accountEmail
+    AccountName               = each.value.accountName
+    ManagedOrganizationalUnit = each.value.managedOrganizationalUnit
+    SSOUserEmail              = each.value.ssoUserEmail
+    SSOUserFirstName          = each.value.ssoUserFirstName
+    SSOUserLastName           = each.value.ssoUserLastName
+  }
+
+  account_tags = each.value.account_tags
+
+  change_management_parameters = {
+    change_requested_by = each.value.changeRequestedBy
+    change_reason       = each.value.changeReason
+  }
+
+  custom_fields = {
+    group = each.value.customFieldsGroup
+  }
+
+  // account_customizations_name = each.value.account_customizations_name
+}
